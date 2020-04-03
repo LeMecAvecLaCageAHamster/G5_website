58893050e3a5792db7ebce3fa74a559db1acd589
Quentin PPÉNAULT <quentinpenault@live.fr>
2020-04-03 15:51:07 +0200

    Added scenario parameter Updated map markers behaviour

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const years = { 'Y1': 2006, 'Y2': 2010, 'Y3': 2020, 'Y4': 2030, 'Y5': 2040, 'Y6': 2050, 'Y7': 2060, 'Y8': 2070, 'Y9': 2080, 'Y10': 2090, 'Y11': 2099 };
 
-var map = L.map('map').setView([0, 0], 2);
-var host = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}'; // default : https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
-var attribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+var map, host, attribution;
+var scenario, year, month;
 
-L.tileLayer(host, {
-	attribution: attribution,
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
-}).addTo(map);
+function initMap() {
+	map = L.map('map').setView([0, 0], 2);
+	host = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}'; // default : https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+	attribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+
+	L.tileLayer(host, {
+		attribution: attribution,
+		subdomains: 'abcd',
+		minZoom: 0,
+		maxZoom: 20,
+		ext: 'png'
+	}).addTo(map);
+}
+
 
 // ------- GeoJSON -------
+
+var geoLayer;
+
 function setGeoInformations() {
+	// To replace existing markers with new ones
+	try {
+		map.removeLayer(geoLayer);
+	} catch (error) {}
+
+	scenario = $("#scenario").val();
+	year = $("#years").val();
+	month = $("#months").val();
+
 	$.get('geojson.json', function (data) {
-		L.geoJSON(data, { onEachFeature: onEachFeature }).addTo(map);
+		geoLayer = L.geoJSON(data, { onEachFeature: onEachFeature }).addTo(map);
 	}, "json");
+
 }
 
-function onEachFeature(feature, layer, scenario = 1, year = 3, month = 1) {
+function onEachFeature(feature, layer) {
 	layer.bindPopup(JSON.stringify(feature.properties["scenario" + scenario]["Y" + year]["M" + month])).openPopup();
-
 }
 
+initMap();
 setGeoInformations();
+
+// -------------------------
