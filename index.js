faa00718af68f55639e9590d9c67c67cd9e3b282
Quentin PÉNAULT <quentinpenault@live.fr>
2020-04-06 14:15:13 +0200

    Added Leaflet "HeatMap" plugin Updated map tiles host Added heat zones on the mapwith temperature informations from geojson

--- index.js
+++ index.js
@@ -1,48 +1,86 @@
 const years = { 'Y1': 2006, 'Y2': 2010, 'Y3': 2020, 'Y4': 2030, 'Y5': 2040, 'Y6': 2050, 'Y7': 2060, 'Y8': 2070, 'Y9': 2080, 'Y10': 2090, 'Y11': 2099 };
 
-var map, host, attribution;
-var scenario, year, month;
+var map, host, attribution, geoData, geoLayer, heatLayer, scenario, year, month;
 
-function initMap() {
-	map = L.map('map').setView([0, 0], 2);
-	host = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}'; // default : https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
-	attribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+// host = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}';
+host = 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+attribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
-	L.tileLayer(host, {
-		attribution: attribution,
-		subdomains: 'abcd',
-		minZoom: 0,
-		maxZoom: 20,
-		ext: 'png'
-	}).addTo(map);
-}
+var layer = L.tileLayer(host, {
+	attribution: attribution,
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+})
 
+// Init map
+map = L.map('map').setView([0, 0], 2);
+map.addLayer(layer);
 
-// ------- GeoJSON -------
-
-var geoLayer;
-
-function setGeoInformations() {
-	// To replace existing markers with new ones
-	try {
-		map.removeLayer(geoLayer);
-	} catch (error) {}
-
-	scenario = $("#scenario").val();
-	year = $("#years").val();
-	month = $("#months").val();
-
+function init() {
+	console.log("----- INITIALIZE MAP -----");
+	
+	// Get data from JSON and init markers & heatmap
 	$.get('geojson.json', function (data) {
-		geoLayer = L.geoJSON(data, { onEachFeature: onEachFeature }).addTo(map);
+		geoData = data;
+		console.log(data);
+		
+
+		scenario = $("#scenario").val();
+		year = $("#years").val();
+		month = $("#months").val();
+
+		// ----- Init Heatmap -----
+		// To replace existing heatmarkers with new ones
+		try {
+			map.removeLayer(heatLayer);
+		} catch (error) { }
+
+		var heatMapData = [];
+		geoData.features.forEach(function (d) {
+			let temperature = d.properties["Y" + year]["scenario" + scenario]["M" + month].temperature;
+			heatMapData.push([d.geometry.coordinates[1], d.geometry.coordinates[0], temperature / 100]);
+			console.log(temperature / 100);
+		});
+
+		heatLayer = L.heatLayer(heatMapData, {
+			minOpacity: 0.5,
+			// gradient: {
+			// 	0: "#000000",
+			// 	0.2: "#570000",
+			// 	0.4: "#ff0000",
+			// 	0.6: "#ffc800",
+			// 	0.8: "#ffff00",
+			// 	1: "#eee"
+			// }
+			gradient: {
+				0: "#000000",
+				0.2: "#570000",
+				0.4: "#ff0000",
+				0.6: "#ffc800",
+				0.8: "#ffff00",
+				1: "#eee"
+			}
+		});
+
+		map.addLayer(heatLayer, { max: 20 });
+
+		// ----- Add markers -----
+		// To replace existing markers with new ones
+		try {
+			map.removeLayer(geoLayer);
+		} catch (error) { }
+
+		geoLayer = L.geoJSON(geoData, { onEachFeature: onEachFeature }).addTo(map);
 	}, "json");
 
 }
+
 
 function onEachFeature(feature, layer) {
 	layer.bindPopup(JSON.stringify(feature.properties["Y" + year]["scenario2"]["M" + month])).openPopup();
 }
 
-initMap();
-setGeoInformations();
-
-// -------------------------
+// ----------------------------------------------
+init();
